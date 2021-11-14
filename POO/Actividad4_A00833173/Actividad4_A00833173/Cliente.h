2b81bfa34b3037225919a4013a6a7d481bc36cda
Jaimedev12 <A00833173@itesm.mx>
2021-11-14 00:03:54 -0600

    Ahí mero va quedando la clase de Cliente

--- POO/Actividad4_A00833173/Actividad4_A00833173/Cliente.h
+++ POO/Actividad4_A00833173/Actividad4_A00833173/Cliente.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
 class Cliente
 {
+private:
+	string nombre;
+	int telefono;
+	string direccion;
+	int horas;
+	int minutos;
+
+public:
+	Cliente();
+	~Cliente();
 };
 

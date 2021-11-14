2b73e1e5027e6759f275b9aebf36f7701a482787
Jaimedev12 <A00833173@itesm.mx>
2021-11-13 23:38:43 -0600

    Actividad 3 instrucciones

--- POO/Actividad4_A00833173/Actividad4_A00833173/Actividad4_A00833173.cpp
+++ POO/Actividad4_A00833173/Actividad4_A00833173/Actividad4_A00833173.cpp
@@ -9,4 +9,25 @@ int main()
     cout << "Hello World!\n";
 }
 
+/*
+Aprovechando su flota de aviones, la aerolínea “Vuela Alto” quiere entrar al mercado
+del envío.Para ello, requieren desarrollar un sistema y se han acercado a ti para que
+les ayudes.Después de varias reuniones, has logrado obtener información del
+siguiente escenario :
+
+El cliente entra al sitio Web de la aerolínea y programa la recolección del envío.El
+cliente ingresará su nombre, teléfono, dirección y horario de la recolección.El sistema
+también registrará el destino(nombre de destinatario, teléfono, dirección), peso y
+dimensiones del paquete.Con todos estos datos capturados, se imprime una etiqueta
+que se debe pegar al paquete y esperar la recepción.
+
+1. Identifica las clases que se describen en el escenario propuesto.
+2. Asegura incluir todos los atributos y métodos de las clases que has
+identificado.
+3. Utiliza la herramienta DIA que has instalado.
+4. Agrega una breve descripción a cada una de las clases que has identificado.
+5. Genera un documento en formato.pdf que contenga el diagrama de clases y
+la información solicitada.Nombra a este documento Actividad_3.pdf
+*/
+
 

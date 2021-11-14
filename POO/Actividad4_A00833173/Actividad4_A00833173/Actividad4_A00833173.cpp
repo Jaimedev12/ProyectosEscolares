71adf158745c4c4fd366e107ea5c8eaf37ce9fbc
Jaimedev12 <A00833173@itesm.mx>
2021-11-13 23:37:41 -0600

    Actividad 3 - crear

---
+++ POO/Actividad4_A00833173/Actividad4_A00833173/Actividad4_A00833173.cpp
@@ -0,0 +1,12 @@
+// Actividad4_A00833173.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
+

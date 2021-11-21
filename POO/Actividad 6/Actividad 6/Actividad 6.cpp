3e5c0d950d81e1d1bcb961d12dfafbb521847ba0
Jaimedev12 <A00833173@itesm.mx>
2021-11-21 15:23:36 -0600

    Actividad 6 empezada

---
+++ POO/Actividad 6/Actividad 6/Actividad 6.cpp
@@ -0,0 +1,11 @@
+// Actividad 6.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+}
+

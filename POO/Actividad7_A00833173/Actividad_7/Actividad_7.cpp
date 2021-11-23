3012405e8624a5b2325ba721fcc769f96a2a27b3
Jaimedev12 <A00833173@itesm.mx>
2021-11-22 22:47:56 -0600

    lloranding

--- POO/Actividad7_A00833173/Actividad_7/Actividad_7.cpp
+++ POO/Actividad7_A00833173/Actividad_7/Actividad_7.cpp
@@ -20,6 +20,28 @@ int mcm(int a, int b) {
     return ((a * b) / MCD(a, b));
 }
 
+void 
+int num1, den1;
+cout << "Enter numerator and denominator of first number: ";
+//user input
+cin >> num1 >> den1;
+int num2, den2;
+cout << "Enter numerator and denominator of second number: ";
+//user input
+cin >> num2 >> den2;
+//finding lcm of the denominators
+int lcm = (den1 * den2) / findGCD(den1, den2);
+//finding the sum of the numbers
+int sum = (num1 * lcm / den1) + (num2 * lcm / den2);
+//normalizing numerator and denominator of result
+int num3 = sum / findGCD(sum, lcm);
+lcm = lcm / findGCD(sum, lcm);
+//printing output
+cout << num1 << "/" << den1 << " + " << num2 << "/" << den2 << " = " << num3 << "/" << lcm;
+return 0;
+
+
+/*
 Fraccion* sumaFracciones(Fraccion* fracc1, Fraccion* fracc2) {
     Fraccion* nuevaFraccion = new Fraccion();
     int nuevoDen, nuevoNum;
@@ -34,6 +56,7 @@ Fraccion* sumaFracciones(Fraccion* fracc1, Fraccion* fracc2) {
 
     return nuevaFraccion;
 }
+*/
 
 //--------------------------------------------------------------------------------------------------------------
 

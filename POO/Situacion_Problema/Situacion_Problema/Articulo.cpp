795c5d9d0e5c34d78754613e57475effeaddccce
Jaimedev12 <A00833173@itesm.mx>
2021-11-30 15:03:03 -0600

    Bases de Articulo

--- POO/Situacion_Problema/Situacion_Problema/Articulo.cpp
+++ POO/Situacion_Problema/Situacion_Problema/Articulo.cpp
@@ -1,1 +1,43 @@
 #include "Articulo.h"
+
+
+//---Constructores y destructores ---------------
+Articulo::Articulo() 
+{
+	this->nombre = "";
+	this->precio = 0;
+}
+
+Articulo::Articulo(string nombre, double precio)
+{
+	this->nombre = nombre;
+	this->precio = precio;
+}
+
+Articulo::~Articulo()
+{
+	delete this;
+}
+
+
+// ---Setters------------------------------------
+void Articulo::setPrecio(double precio) 
+{
+	this->precio = precio;
+}
+
+void Articulo::setNombre(string nombre)
+{
+	this->nombre = nombre;
+}
+
+//---Getters-------------------------------------
+double Articulo::getPrecio()
+{
+	return precio;
+}
+
+string Articulo::getNombre()
+{
+	return nombre;
+}
--- POO/Situacion_Problema/Situacion_Problema/Articulo.h
+++ POO/Situacion_Problema/Situacion_Problema/Articulo.h
@@ -1,5 +1,24 @@
 #pragma once
+#include <string>
+using namespace std;
+
 class Articulo
 {
+private:
+	double precio;
+	string nombre;
+public:
+	Articulo();
+	Articulo(string nombre, double precio);
+	~Articulo();
+
+	// ---Setters------------------------------------
+	void setPrecio(double precio);
+	void setNombre(string nombre);
+
+	//---Getters-------------------------------------
+	double getPrecio();
+	string getNombre();
+
 };
 

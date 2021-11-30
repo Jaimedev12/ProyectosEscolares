d2a0f0117a15bdad0bb938470365ea1c0c8e5408
Jaimedev12 <A00833173@itesm.mx>
2021-11-30 16:17:52 -0600

    Las bases de la clase de Cliente

--- POO/Situacion_Problema/Situacion_Problema/Cliente.cpp
+++ POO/Situacion_Problema/Situacion_Problema/Cliente.cpp
@@ -1,1 +1,67 @@
 #include "Cliente.h"
+
+//---Constructores y destructores ---------------
+Cliente::Cliente()
+{
+	this->nombre = "";
+	this->articulosEnCarrito = 0;
+	this->articulosMaximosEnCarrito = 10;
+}
+
+Cliente::Cliente(string nombre)
+{
+	this->nombre = nombre;
+	this->articulosEnCarrito = 0;
+	this->articulosMaximosEnCarrito = 10;
+}
+
+Cliente::~Cliente()
+{
+	delete this;
+}
+
+//---Setters-----------------------------------
+void Cliente::setNombre(string nombre)
+{
+	this->nombre = nombre;
+}
+
+//---Getters-----------------------------------
+string Cliente::getNombre()
+{
+	return nombre;
+}
+
+Articulo** Cliente::getArticulosEnCarrito()
+{
+	return articulosEnCarrito;
+}
+
+double Cliente::getTotalAPagar()
+{
+	double total = 0;
+	for (int i = 0; i < this->numeroDeArticulosEnCarrito; i++)
+	{
+		total += articulosEnCarrito[i]->getPrecio();
+	}
+	return total;
+}
+
+//---Funciones Especiales----------------------
+void Cliente::agregarArticuloAlCarrito(Articulo* articulo)
+{
+	if (this->numeroDeArticulosEnCarrito == this->articulosMaximosEnCarrito)
+	{
+		this->numeroDeArticulosEnCarrito = this->articulosMaximosEnCarrito + 10;
+		Articulo** temp = new Articulo* [this->articulosMaximosEnCarrito];
+		for (int i = 0; i < this->numeroDeArticulosEnCarrito; i++)
+		{
+			temp[i] = this->articulosEnCarrito[i];
+		}
+		delete[] this->articulosEnCarrito;
+		this->articulosEnCarrito = temp;
+	}
+
+	this->articulosEnCarrito[this->numeroDeArticulosEnCarrito] = articulo;
+	this->articulosEnCarrito++;
+}
--- POO/Situacion_Problema/Situacion_Problema/Cliente.h
+++ POO/Situacion_Problema/Situacion_Problema/Cliente.h
@@ -1,5 +1,32 @@
 #pragma once
+#include "Articulo.h"
+#include <string>
+using namespace std;
+
 class Cliente
 {
+private:
+	string nombre;
+	int numeroDeArticulosEnCarrito;
+	int articulosMaximosEnCarrito;
+	Articulo** articulosEnCarrito;
+
+public:
+	Cliente();
+	Cliente(string nombre);
+	~Cliente();
+
+	//---Setters-----------------------------------
+	void setNombre(string nombre);
+
+	//---Getters-----------------------------------
+	string getNombre();
+	Articulo** getArticulosEnCarrito();
+	double getTotalAPagar();
+
+	//---Funciones Especiales----------------------
+	void agregarArticuloAlCarrito(Articulo* articulo);
+
+
 };
 
